2edc3c5aa55408113b1658ee775189dc46b75513
Mahmoud <mmshehatta@gmail.com>
2021-04-27 09:03:53 +0200

    Adding models.py in User App

---
+++ users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from phone_field import PhoneField
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+
+# Create your models here.
+
+class User(AbstractUser):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length = 254, unique=True)
+    password = models.CharField(max_length=255)
+
+    # phone = PhoneField(blank=True, help_text='Contact phone number')
+    # image = models.ImageField(upload_to='users/UserProfile/images' , null=True , blank=True)
+
+
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+    # def __str__(self):
+    #     return '%s' % self.user
+
+    # def create_profile(sender, **kwargs):
+    #     if kwargs['created']:
+    #         user_profile = UserProfile.objects.create(user=kwargs['instance'])
+    # post_save.connect(create_profile, sender=User)
+
